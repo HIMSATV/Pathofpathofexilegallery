837120373cd51240d344203491b214dc549f9667
HIMSATV <hosukgun@naver.com>
2026-03-01 09:57:54 +0900

    Delete v2/app.js

--- v2/app.js
+++
@@ -1,5 +0,0 @@
-// app.js (v2) - SAFE BOOT
-// game.js가 자체적으로 init/loop를 돌리는 구조를 존중하고, resize만 보조한다.
-window.addEventListener("load", function () {
-  try { if (typeof resize === "function") resize(); } catch(_) {}
-});
